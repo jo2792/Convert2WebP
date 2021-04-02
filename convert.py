deebd508357b282e75a108002890724d5e2abf42
Jordan Alwon <jordan.alwon@student.jade-hs.de>
2021-04-02 14:20:22 +0200

    basic function done

---
+++ convert.py
@@ -0,0 +1,20 @@
+import os
+from PIL import Image
+from tqdm import tqdm
+
+path = os.getcwd()
+
+if not os.path.isdir(os.path.join(path,'Input')):
+    print("Initialize I/O folders")
+    os.mkdir(os.path.join(path,'Input'))
+if not os.path.isdir(os.path.join(path,'Output')):
+    os.mkdir(os.path.join(path,'Output'))
+
+file_list = os.listdir(os.path.join(path,'Input'))
+
+for file in tqdm(file_list, disable=not len(file_list), unit='Image', desc="Convert"):
+    file_path = os.path.join(path,'Input',file)
+    file_destination = os.path.join(path,'Output',file.rsplit('.')[0]+'.webp')
+
+    img = Image.open(file_path)
+    img.save(file_destination, format = "WebP", lossless = True)
